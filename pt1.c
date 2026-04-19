3d57687fac375043cee87505f0ed8e1b1e76c757
vixi <vixi@ksu.edu>
2026-04-19 13:36:47 -0500

    pt1.c: Added batching to pthreads implementation

--- pt1.c
+++ pt1.c
@@ -1,124 +1,219 @@
+#include <fcntl.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
+#ifndef NUM_THREADS
 #define NUM_THREADS 4
+#endif
 
-#define ARRAY_SIZE 2000000
-#define STRING_SIZE 16
-#define ALPHABET_SIZE 26
+#define BATCH_SIZE 32768
+#define MAX_LINE 8192
 
-pthread_mutex_t mutexsum; // mutex for char_counts
+// We ever need one copy of this data, so in main we have an instance of batch
+// wihch we pass every thread a pointer to
+typedef struct {
+  char buf[BATCH_SIZE];
+  int len;
+  int bytes_read;
+} Batch;
 
-char char_array[ARRAY_SIZE][STRING_SIZE];
-int char_counts[ALPHABET_SIZE]; // count of individual characters
+typedef struct {
+  int thread_index;
+  Batch *batch;
+  int *max_chars; // Write results here
+  int max_chars_capacity;
+  int max_chars_len;
+} ThreadArg;
 
-char getRandomChar() {
-  int randNum = 0;
-  char randChar = ' ';
+const int bytes_per_thread = (BATCH_SIZE + NUM_THREADS - 1) / NUM_THREADS;
 
-  randNum =
-      ALPHABET_SIZE * (rand() / (RAND_MAX + 1.0)); // pick number 0 < # < 25
-  randNum = randNum + 97;                          // scale to 'a'
-  randChar = (char)randNum;
-
-  // printf("%c", randChar);
-  return randChar;
-}
-
-void init_arrays() {
-  int i, j;
-
-  pthread_mutex_init(&mutexsum, NULL);
-
-  for (i = 0; i < ARRAY_SIZE; i++) {
-    for (j = 0; j < STRING_SIZE; j++) {
-      char_array[i][j] = getRandomChar();
+// Appends max_char to arg->max_chars; reallocating max_chars as needed
+static void append_max_char(int max_char, ThreadArg *arg) {
+  if (arg->max_chars_len >= arg->max_chars_capacity) {
+    // If max_chars is at capacity
+    // then realloc(max_chars, ...) to double its capacity
+    arg->max_chars_capacity *= 2;
+    arg->max_chars =
+        (int *)realloc(arg->max_chars, arg->max_chars_capacity * sizeof(int));
+    if (NULL == arg->max_chars) {
+      printf("Error: realloc failed\n");
+      exit(1);
     }
   }
-
-  for (i = 0; i < ALPHABET_SIZE; i++) {
-    char_counts[i] = 0;
-  }
+  arg->max_chars[arg->max_chars_len] = max_char;
+  arg->max_chars_len += 1;
 }
 
-void *count_array(void *myID) {
-  char theChar;
-  int i, j, charLoc;
-  int local_char_count[ALPHABET_SIZE];
+// Each thread begins execution here and is assigned a specific region of the
+// batch for which it is responsible. Each thread's responsibility begins just
+// after the first newline in their partition, and extends to the first newline
+// in next thread's partition (besides the 0'th thread, see below).
+static void *thread_routine(void *thread_args) {
+  ThreadArg *arg = (ThreadArg *)thread_args;
+  Batch *batch = arg->batch;
 
-  int startPos = ((int)myID) * (ARRAY_SIZE / NUM_THREADS);
-  int endPos = startPos + (ARRAY_SIZE / NUM_THREADS);
-
-  printf("myID = %d startPos = %d endPos = %d \n", (int)myID, startPos, endPos);
-
-  // init local count array
-  for (i = 0; i < ALPHABET_SIZE; i++) {
-    local_char_count[i] = 0;
+  int char_index = bytes_per_thread * arg->thread_index;
+  const int begining_of_next_threads_partition = char_index + bytes_per_thread;
+  // The 0'th thread is responsible for the line starting at index 0.
+  // All other threads need to advance to the next newline; they're lines start
+  // there
+  if (0 != arg->thread_index) {
+    for (; char_index < batch->len && batch->buf[char_index] != '\n';
+         char_index += 1)
+      ;
+    // Add one to skip over the newline which the loop stopped at
+    char_index += 1;
   }
-  // count up our section of the global array
-  for (i = startPos; i < endPos; i++) {
-    for (j = 0; j < STRING_SIZE; j++) {
-      theChar = char_array[i][j];
-      charLoc = ((int)theChar) - 97;
-      local_char_count[charLoc]++;
+
+  unsigned char max_char = 0;
+  // Foreach character up to the end of batch->buf
+  while (char_index < batch->len) {
+    // Note that any threads given partitions beyond the end of batch->buf
+    // will not have entered into this loop
+    if (batch->buf[char_index] == '\n') {
+      // Upon newline, we have to append max_char to max_chars
+      append_max_char(max_char, arg);
+
+      // Reset max_char back to zero for the next line
+      max_char = 0;
+
+      if (char_index >= begining_of_next_threads_partition) {
+        // If this newline was in the next threads partition,
+        // then we're done! (unless we're at EOF, see below).
+        // Note although this line ended in the next threads partition,
+        // it is still this thread's responsibility (see function comment)
+        break;
+      }
+    } else if ((unsigned char)batch->buf[char_index] > max_char) {
+      // If this char is the greatest yet to be seen,
+      // then set it as the current max_char
+      max_char = (unsigned char)batch->buf[char_index];
     }
+    char_index += 1;
   }
-  // sum up the partial counts into the global arrays
-  pthread_mutex_lock(&mutexsum);
-  for (i = 0; i < ALPHABET_SIZE; i++) {
-    char_counts[i] += local_char_count[i];
+
+  if (0 == batch->bytes_read && char_index == batch->len - 1 &&
+      batch->buf[char_index] != '\n') {
+    // If we bytes_read is zero, then we are at EOF.
+    // If we are the last thread, then we should be the only thread where
+    // char_index points to the very last char.
+    // If the last char is not '\n',
+    // then the last line is not terminate by a '\n',
+    // therefore we didn't handle in the above loop.
+    // Seemingly we should still handle this and push the current max_char
+    append_max_char(max_char, arg);
   }
-  pthread_mutex_unlock(&mutexsum);
 
   pthread_exit(NULL);
 }
 
-void print_results() {
-  int i, j, total = 0;
+int main(int argc, char *argv[]) {
+  const char *filepath = "/homes/eyv/cis520/wiki_dump.txt";
+  int fd = open(filepath, O_RDONLY);
 
-  // then print out the totals
-  for (i = 0; i < ALPHABET_SIZE; i++) {
-    total += char_counts[i];
-    printf(" %c %d\n", (char)(i + 97), char_counts[i]);
+  if (-1 == fd) {
+    printf("Error: Could not open file %s\n", filepath);
+    exit(1);
   }
-  printf("\nTotal characters:  %d\n", total);
-}
 
-main() {
-  int i, rc;
-  pthread_t threads[NUM_THREADS];
+  // Initialize and set thread detached attribute
   pthread_attr_t attr;
-  void *status;
-
-  /* Initialize and set thread detached attribute */
   pthread_attr_init(&attr);
   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-  init_arrays();
+  // Initialize batch
+  Batch batch;
+  batch.len = 0;
+  batch.bytes_read = 0;
 
-  for (i = 0; i < NUM_THREADS; i++) {
-    rc = pthread_create(&threads[i], &attr, count_array, (void *)i);
-    if (rc) {
-      printf("ERROR; return code from pthread_create() is %d\n", rc);
-      exit(-1);
+  // Create threads
+  pthread_t threads[NUM_THREADS];
+  ThreadArg thread_args[NUM_THREADS];
+  const int initial_thread_max_chars_size = 64;
+  for (int thread_index = 0; thread_index < NUM_THREADS; thread_index += 1) {
+    thread_args[thread_index].thread_index = thread_index;
+    thread_args[thread_index].batch = &batch;
+    thread_args[thread_index].max_chars =
+        malloc(initial_thread_max_chars_size * sizeof(int));
+    if (NULL == thread_args[thread_index].max_chars) {
+      printf("Error: malloc failed\n");
+      exit(1);
     }
+    thread_args[thread_index].max_chars_capacity =
+        initial_thread_max_chars_size;
+    thread_args[thread_index].max_chars_len = 0;
   }
 
-  /* Free attribute and wait for the other threads */
-  pthread_attr_destroy(&attr);
-  for (i = 0; i < NUM_THREADS; i++) {
-    rc = pthread_join(threads[i], &status);
-    if (rc) {
-      printf("ERROR; return code from pthread_join() is %d\n", rc);
-      exit(-1);
+  int lines_processed = 0;
+
+  // This tracks how many bytes of batch are currently filled.
+  // Those filled bytes should always be at the front of batch.
+  for (;;) {
+    // Read into batch starting at where the currently filled bytes end.
+    // Read as many bytes as we can currently fit up to the end of batch.
+    batch.bytes_read = read(fd, batch.buf + batch.len, BATCH_SIZE - batch.len);
+    // Add those bytes we just read to track the currently filled bytes
+    batch.len += batch.bytes_read;
+
+    for (int thread_index = 0; thread_index < NUM_THREADS; thread_index += 1) {
+      int pthread_create_error_number =
+          pthread_create(&threads[thread_index], &attr, thread_routine,
+                         (void *)&thread_args[thread_index]);
+      if (0 != pthread_create_error_number) {
+        printf("ERROR; return code from pthread_create() is %d\n",
+               pthread_create_error_number);
+        exit(-1);
+      }
     }
+
+    for (int thread_index = 0; thread_index < NUM_THREADS; thread_index += 1) {
+      void *status;
+      int pthread_join_error_number =
+          pthread_join(threads[thread_index], &status);
+      if (pthread_join_error_number) {
+        printf("ERROR; error number from pthread_join() is %d\n",
+               pthread_join_error_number);
+        exit(-1);
+      }
+    }
+
+    // Print results in order
+    for (int thread_index = 0; thread_index < NUM_THREADS; thread_index += 1) {
+      for (int i = 0; i < thread_args[thread_index].max_chars_len; i += 1) {
+        printf("%d: %d\n", lines_processed,
+               thread_args[thread_index].max_chars[i]);
+        lines_processed += 1;
+      }
+      // Reset the len of this thread's max_chars
+      thread_args[thread_index].max_chars_len = 0;
+    }
+
+    // If we've just handled the last batch that was left over from the last
+    // read, then we're done!
+    if (0 == batch.bytes_read) {
+      break;
+    }
+
+    // Find the last newline in batch,
+    // we haven't processed anything after that yet.
+    int last_newline_index = batch.len - 1;
+    for (;; last_newline_index -= 1) {
+      if (batch.buf[last_newline_index] == '\n') {
+        break;
+      }
+    }
+    // If the newline is the last char in batch, we can just set batch_len = 0
+    // Otherwise we have to move the unprocessed bytes to the front of the
+    // buffer and update batch_len accordingly.
+    int bytes_to_move = batch.len - last_newline_index - 1;
+    if (last_newline_index != BATCH_SIZE - 1) {
+      memmove(batch.buf, batch.buf + last_newline_index + 1, bytes_to_move);
+    }
+    batch.len = bytes_to_move;
   }
 
-  print_results();
-
-  pthread_mutex_destroy(&mutexsum);
-  printf("Main: program completed. Exiting.\n");
-  pthread_exit(NULL);
+  close(fd);
 }
