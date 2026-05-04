39a2977a564472dcb96cafccd8a92cd388027bca
vixi <vixi@ksu.edu>
2026-05-03 22:28:56 -0500

    Replace current files with Zeke's files

--- 3way-mpi/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# run.sh {num_threads}
-mpirun -N $1 ./3way-mpi-$1
-
--- 3way-mpi/schedule.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# ./schedule.sh {time} {mem-per-cpu} {num_nodes}
-make -s NUM_THREADS=$3 && sbatch --time=$1 --mem-per-cpu=$2 --cpus-per-task=1 --ntasks=$3 --nodes=$4 --constraint=moles ./run.sh $3
-
